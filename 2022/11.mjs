db20c554c9d1ab464d7bc6fdd1bda709d2aa9ef0
Hristo Dokov <hdokov@gmail.com>
2022-12-14 16:43:42 +0200

    Day 11

--- 2022/11.mjs
+++ 2022/11.mjs
@@ -10,7 +10,7 @@ Test: divisible by 23
 
 Monkey 1:
 Starting items: 54, 65, 75, 74
-Operation: new = old + (6/100)
+Operation: new = old + 6
 Test: divisible by 19
   If true: throw to monkey 2
   If false: throw to monkey 0
@@ -24,7 +24,7 @@ Test: divisible by 13
 
 Monkey 3:
 Starting items: 74
-Operation: new = old + (3/100)
+Operation: new = old + 3
 Test: divisible by 17
   If true: throw to monkey 0
   If false: throw to monkey 1`
@@ -64,16 +64,11 @@ console.log(
     .reduce((a, b) => a * b, 1)
 )
 
-const monkeys2 = input.split('\n\n').map(s => {
+const monkeys2 = data.split('\n\n').map(s => {
   const [name, items, operation, test, t, f] = s.split('\n')
   return {
     name: name.split(':').first().toLowerCase(),
-    items: items
-      .split(': ')
-      .last()
-      .split(',')
-      .map(Number)
-      .map(i => i / 100),
+    items: items.split(': ').last().split(',').map(Number),
     operation: operation.split('new = ').last(),
     test: Number(test.split('by ').last()),
     t: t.split('to ').last(),
@@ -82,28 +77,26 @@ const monkeys2 = input.split('\n\n').map(s => {
   }
 })
 
+const denominator = monkeys2.map(m => m.test).reduce((a, b) => a * b, 1)
 //console.log(monkeys2, eval('(3/100)'))
-const start = new Date()
-for (let k = 0; k < 20; k++) {
+for (let k = 0; k < 10000; k++) {
   monkeys2.forEach(m => {
     let old = m.items.shift()
     while (old) {
-      const n = eval(m.operation)
-      let nn = false
-      if (n < 100) {
-        nn = Math.floor((n * 100) % m.test) === 0
-      } else {
-        nn = Math.floor(n % (m.test / 100)) === 0
+      let n = eval(m.operation)
+      const next = n % m.test === 0 ? m.t : m.f
+
+      if (n > denominator) {
+        n = n % denominator
       }
-      const next = nn ? m.t : m.f
-      console.log(old, n, next)
+
       monkeys2.find(m1 => m1.name === next).items.push(n)
       m.inspects += 1
       old = m.items.shift()
     }
   })
 }
-console.log(new Date() - start)
+//console.log(new Date() - start)
 console.log(
   'P2:',
   monkeys2.map(m => `${m.name}: ${m.inspects}`),
